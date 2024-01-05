af7a7b6d6d1505a4090da8ee42f6a559fdaecc96
elbobyhn <joseluis.hn@gmail.com>
2024-01-04 20:19:14 -0600

    BED SIZE

--- Configuration.h
+++ Configuration.h
@@ -1703,12 +1703,12 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 220 //Custom Elboby
-#define Y_BED_SIZE 220 //Custom Elboby
+#define X_BED_SIZE 225 //Custom Elboby
+#define Y_BED_SIZE 225 //Custom Elboby
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS -10 
+#define Y_MIN_POS 0 
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
